22500673998d5875abcaf552c7e494bb4f426a53
micromouse <eggeggak@vip.qq.com>
2023-12-14 18:35:31 +0800

    add actuator supports

--- web/src/main/java/com/studies/myfirstspringbootapp/WebApplication.java
+++ web/src/main/java/com/studies/myfirstspringbootapp/WebApplication.java
@@ -3,6 +3,7 @@ package com.studies.myfirstspringbootapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.StringUtils;
 import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
@@ -12,14 +13,13 @@ public class WebApplication {
         ConfigurableApplicationContext context = SpringApplication.run(WebApplication.class, args);
 
         Jedis jedis = context.getBean(Jedis.class);
-        //System.out.println(jedis);
+        System.out.println(jedis);
 
-        //设置值
-        jedis.set("hello", "world");
-
-        //获取值
-        String value = jedis.get("hello");
-        System.out.printf("key hellow value is : %s%n", value);
+        String value = jedis.getSet("hello", "world");
+        if (value == null) {
+            value = "world";
+        }
+        System.out.printf("key hello value is : %s%n", value);
     }
 
 }
