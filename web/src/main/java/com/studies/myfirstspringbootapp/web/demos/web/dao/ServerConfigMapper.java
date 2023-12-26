378ad4da661c9a05b2a2a1e62087fe7859878865
micromouse <eggeggak@vip.qq.com>
2023-12-26 16:23:42 +0800

    add multiple mybatis features

--- web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/dao/ServerConfigMapper.java
+++ web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/dao/ServerConfigMapper.java
@@ -3,6 +3,7 @@ package com.studies.myfirstspringbootapp.web.demos.web.dao;
 import com.studies.myfirstspringbootapp.web.demos.web.models.ServerConfig;
 import org.apache.ibatis.annotations.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -27,7 +28,6 @@ public interface ServerConfigMapper {
      *
      * @return ：服务器配置信息集合
      */
-    @Select("SELECT * FROM ServerConfig ORDER BY `key` ASC")
     List<ServerConfig> list();
 
     /**
@@ -36,8 +36,47 @@ public interface ServerConfigMapper {
      * @param key : 服务器配置key
      * @return ：服务器配置对象
      */
+    @Results({
+            @Result(column = "datachange_createdby", property = "dataChangeCreatedBy"),
+            @Result(column = "datachange_createdtime", property = "dataChangeCreatedTime")
+    })
     @Select("SELECT * FROM ServerConfig WHERE `key` = #{key}")
     ServerConfig findByKey(@Param("key") String key);
+
+    /**
+     * 按key集合查找服务器配置
+     *
+     * @param keys ：key集合
+     * @return ：服务器配置集合
+     */
+    List<ServerConfig> findByKeys(List<String> keys);
+
+    /**
+     * 查询符合条件的服务器配置
+     * -- 可以使用下列方式拼接字符串，这样预编译无效[key like '%#{key}%']
+     * select * from serverconfig where key like '%${key}%'
+     *
+     * @param key              ：配置key
+     * @param createdTimeStart ：数据变动建立时间开始
+     * @param createdTimeEnd   ：数据变动建立时间结束
+     * @return ：服务器配置信息集合
+     */
+    @Select("SELECT * " +
+            "FROM   serverconfig " +
+            "WHERE  datachange_createdtime>=#{createdTimeStart} AND " +
+            "       datachange_createdtime<#{createdTimeEnd} AND " +
+            "       `key` LIKE CONCAT('%',#{key},'%')")
+    List<ServerConfig> query(String key, LocalDate createdTimeStart, LocalDate createdTimeEnd);
+
+    /**
+     * 按mybatis xml配置映射方式查询服务器配置
+     *
+     * @param key              ：配置key
+     * @param createdTimeStart ：数据变动建立时间开始
+     * @param createdTimeEnd   ：数据变动建立时间结束
+     * @return ：服务器配置信息集合
+     */
+    List<ServerConfig> queryByXml(String key, LocalDate createdTimeStart, LocalDate createdTimeEnd);
 
     /**
      * 按id删除服务器配置
--- web/src/test/java/com/studies/myfirstspringbootapp/web/ServerConfigMapperTest.java
+++ web/src/test/java/com/studies/myfirstspringbootapp/web/ServerConfigMapperTest.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 服务器配置映射器测试
@@ -27,13 +31,64 @@ public class ServerConfigMapperTest {
     }
 
     /**
+     * 成功获得所有服务器配置
+     */
+    @Test
+    public void get_all_ServerConfig_Success() {
+        List<ServerConfig> serverConfigs = serverConfigMapper.list();
+        Assertions.assertEquals(5, serverConfigs.size());
+    }
+
+    /**
      * 按key查找服务器配置成功
      */
     @Test
     public void find_ServerConfig_By_Key_Success() {
         ServerConfig serverConfig = serverConfigMapper.findByKey("eureka.service.url");
         Assertions.assertNotNull(serverConfig);
+        Assertions.assertEquals(LocalDateTime.parse("2023-11-21T03:42:02"), serverConfig.getDataChangeCreatedTime());
         Assertions.assertEquals("eureka.service.url", serverConfig.getKey());
+    }
+
+    /**
+     * 按key集合查找服务器配置成功
+     */
+    @Test
+    public void find_ServerConfig_By_Keys_Success() {
+        List<String> keys = Arrays.asList("item.key.length.limit", "item.value.length.limit");
+        List<ServerConfig> serverConfigs = serverConfigMapper.findByKeys(keys);
+        Assertions.assertEquals(2, serverConfigs.size());
+    }
+
+    /**
+     * 查询符合条件的服务器配置
+     */
+    @Test
+    public void query_ServerConfig_Success() {
+        LocalDate createdTimeStart = LocalDate.parse("2023-01-01");
+        LocalDate createdTimeEnd = LocalDate.parse("2023-12-01");
+        List<ServerConfig> serverConfigs = serverConfigMapper.query("item", createdTimeStart, createdTimeEnd);
+        Assertions.assertEquals(2, (long) serverConfigs.size());
+    }
+
+    /**
+     * 按mybatis xml配置映射方式查询符合条件的服务器配置
+     */
+    @Test
+    public void query_ServerConfig_By_Xml_Success() {
+        LocalDate createdTimeStart = LocalDate.parse("2023-01-01");
+        LocalDate createdTimeEnd = LocalDate.parse("2023-12-01");
+        List<ServerConfig> serverConfigs = serverConfigMapper.queryByXml("item", createdTimeStart, createdTimeEnd);
+        Assertions.assertEquals(2, (long) serverConfigs.size());
+    }
+
+    /**
+     * 按mybatis xml配置映射方式查询符合条件的服务器配置
+     */
+    @Test
+    public void query_serverConfig_By_Xml_Optional_Condition_Success() {
+        List<ServerConfig> serverConfigs = serverConfigMapper.queryByXml("", null, null);
+        Assertions.assertEquals(5, (long) serverConfigs.size());
     }
 
     /**
@@ -41,7 +96,7 @@ public class ServerConfigMapperTest {
      */
     @Test
     public void delete_ServerConfig_By_Id_Success() {
-        Integer count = serverConfigMapper.DeleteById(11);
+        Integer count = serverConfigMapper.DeleteById(12);
         Assertions.assertEquals(1, count, "0行服务器配置被删除");
     }
 
