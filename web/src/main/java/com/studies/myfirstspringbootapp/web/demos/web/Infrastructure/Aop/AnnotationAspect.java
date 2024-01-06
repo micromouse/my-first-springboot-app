ce095730a3be603c377412f3bd31ea9a3498ea94
micromouse <eggeggak@vip.qq.com>
2024-01-06 17:38:58 +0800

    update annotationaspect

--- web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/Infrastructure/Aop/AnnotationAspect.java
+++ web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/Infrastructure/Aop/AnnotationAspect.java
@@ -1,6 +1,7 @@
 package com.studies.myfirstspringbootapp.web.demos.web.Infrastructure.Aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -20,9 +21,13 @@ public class AnnotationAspect {
 
     /**
      * 注解[MyAnnotation]方法执行前
+     *
+     * @param joinPoint : 连接点
      */
     @Before("common()")
-    public void before(){
-        log.info("注解[MyAnnotation] before ...");
+    public void before(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        log.info("注解[MyAnnotation] 方法[{}.{}] before ...", className, methodName);
     }
 }
