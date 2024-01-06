2389edf7c4e04284bee72ba12a2711b8f329ff69
micromouse <eggeggak@vip.qq.com>
2024-01-06 14:57:45 +0800

    add adviceaspect

---
+++ web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/Infrastructure/Aop/AdviceAspect.java
@@ -0,0 +1,81 @@
+package com.studies.myfirstspringbootapp.web.demos.web.Infrastructure.Aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * Aspect通知
+ */
+@Order(1)
+@Slf4j
+@Component
+@Aspect
+public class AdviceAspect {
+    @Pointcut("execution(* com.studies.myfirstspringbootapp.web.demos.web.controllers.*.*(..)) ")
+    public void commonPointcut() {
+    }
+
+    /**
+     * 方法执行前通知
+     */
+    @Before("commonPointcut()")
+    public void before() {
+        log.info("before ...");
+    }
+
+    /**
+     * 方法环绕通知(方法执行前/后通知)
+     * ---------------------------------
+     * 正常请求http://localhost:8081/login顺序执行下列通知
+     * around before ...
+     * before ...
+     * afterReturning ...
+     * after ...
+     * around after ...
+     * ---------------------------------
+     * 请求http://localhost:8081/login抛出异常时顺序执行下列通知
+     * around before ...
+     * before ...
+     * afterThrowing ...
+     * after ...
+     * ---------------------------------
+     *
+     * @param joinPoint : 处理切入点
+     */
+    @Around("commonPointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("around before ...");
+
+        Object obj = joinPoint.proceed();
+        log.info("around after ...");
+
+        return obj;
+    }
+
+    /**
+     * 方法执行后通知，无论是否有异常都会通知
+     */
+    @After("commonPointcut()")
+    public void after() {
+        log.info("after ...");
+    }
+
+    /**
+     * 方法正常执行后通知,有异常不会执行
+     */
+    @AfterReturning("commonPointcut()")
+    public void afterReturning() {
+        log.info("afterReturning ...");
+    }
+
+    /**
+     * 方法执行异常后通知
+     */
+    @AfterThrowing("execution(* com.studies.myfirstspringbootapp.web.demos.web.controllers.*.*(..))")
+    public void afterThrowing() {
+        log.info("afterThrowing ...");
+    }
+}
--- web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/Infrastructure/Aop/StopwatchAspect.java
+++ web/src/main/java/com/studies/myfirstspringbootapp/web/demos/web/Infrastructure/Aop/StopwatchAspect.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
  * 执行操作运行时间记录
  */
+@Order(2)
 @Slf4j
 @Component
 @Aspect
@@ -20,7 +22,7 @@ public class StopwatchAspect {
      * @return ：控制器方法返回结果
      * @throws Throwable ：向上抛出异常
      */
-    @Around("execution(* com.studies.myfirstspringbootapp.web.demos.web.controllers.*.*(..))")
+    @Around("com.studies.myfirstspringbootapp.web.demos.web.Infrastructure.Aop.AdviceAspect.commonPointcut()")
     public Object StopwatchByController(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         try {
@@ -29,6 +31,5 @@ public class StopwatchAspect {
             long end = System.currentTimeMillis();
             log.info("当前请求[{}]耗时:{}ms", joinPoint.getSignature(), end - start);
         }
-
     }
 }
